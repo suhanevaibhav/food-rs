737093659a493e1a8a328f041b8e6cf425600707
suhanevaibhav <shri.vaibhavsuhane@gmail.com>
2022-09-13 11:46:49 +0530

    intial commits

---
+++ src/main/java/com/suhane/food/FoodRsApplication.java
@@ -0,0 +1,13 @@
+package com.suhane.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suhane/food/FoodRsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhane.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
